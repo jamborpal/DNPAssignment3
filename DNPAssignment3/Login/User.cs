43fea5157712eb1ea69276dcd8ac472ce5173d02
jamborpal <58312465+jamborpal@users.noreply.github.com>
2020-11-07 23:04:47 +0100

    Fully working

---
+++ DNPAssignment3/Login/User.cs
@@ -0,0 +1,15 @@
+﻿﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Assignment2.Login
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+    }
+}
